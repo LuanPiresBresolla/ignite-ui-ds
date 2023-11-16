50afe0019b429d42badc13bd6c6b2f8229206d10
Luan Bresolla <luanpires34@gmail.com>
2023-11-16 00:50:28 -0300

    adjusts in project

--- packages/react/src/components/Checkbox/styles.ts
+++ packages/react/src/components/Checkbox/styles.ts
@@ -19,10 +19,9 @@ export const CheckboxContainer = styled(Checkbox.Root, {
 
   '&[data-state="checked"]': {
     background: '$ignite300',
-    borderColor: '$ignite300',
   },
 
-  '&:focus': {
+  '&:focus, &[data-state="checked"]': {
     border: '2px solid $ignite300',
   },
 })
